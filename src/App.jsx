48999e9eb314463771a20dea4a04bc495bf1a38d
AndreaaGM <moon.lp.13@gmail.com>
2024-08-12 20:05:54 -0600

    subiendo proyecto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Card from './Component/Card'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Carga de estudiantes</h1>
+      <form action=""></form>
+      <Card/>
+    </>
+  )
+}
+
+export default App
